577fddcabe5eaaf69e029bcc84b1b2187d6e6e2c
Raphael Pinto <raphaelllpinto@gmail.com>
2024-06-26 15:15:02 -0300

    added env variables

--- lullabylore/stories/firebase_init.py
+++ lullabylore/stories/firebase_init.py
@@ -1,8 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import os
+from environ import Env
 
-cred_path = os.path.join(os.path.dirname(__file__), 'lullaby-lore-firebase-adminsdk-v51wx-003198aeaa.json')
+env = Env()
+env.read_env()
+
+cred_path = env('FIREBASE_CRED_PATH')
+cred_path = os.path.join(os.path.dirname(__file__), cred_path)
                     
 cred = credentials.Certificate(cred_path)
 firebase_admin.initialize_app(cred)
