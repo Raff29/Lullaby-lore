888f58f6f2fd1ea33f5da85dd52d342563687107
Raphael Pinto <60246144+Raff29@users.noreply.github.com>
2024-06-28 22:21:06 -0300

    added receiver method to save this new fieled when django creates a new user

--- lullabylore/users/models.py
+++ lullabylore/users/models.py
@@ -1,18 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from django.utils import timezone
 
+
 class FavouriteStories(models.Model):
-  firebase_story_id = models.CharField(max_length=255, unique=True)
-  title = models.CharField(max_length=255, blank=True)
-  created_at = models.DateTimeField(default=timezone.now)
-  
-  def __str__(self):
-    return self.title or self.firebase_story_id
+    firebase_story_id = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title or self.firebase_story_id
+
 
 class UserProfile(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  favourite_stories = models.ManyToManyField(FavouriteStories,blank=True)
-  
-  def __str__(self):
-    return self.user.username
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favourite_stories = models.ManyToManyField(FavouriteStories, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+post_save.connect(create_user_profile, sender=User)
