3b6320f8823305eb19143ddb3eb297ddaf84be08
Raphael Pinto <raphaelllpinto@gmail.com>
2024-07-02 10:26:59 -0300

    registered two models to django's admin view for easier management

--- lullabylore/users/admin.py
+++ lullabylore/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import UserProfile, FavouriteStories
 
-# Register your models here.
+
+class userProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'get_favourite_stories')
+    search_fields = ('user__username',)
+
+    def get_favourite_stories(self, obj):
+        return ", ".join([fav.title for fav in obj.favourite_stories.all()])
+    get_favourite_stories.short_description = 'Favourite Stories'
+
+
+class FavouriteStoriesAdmin(admin.ModelAdmin):
+    list_display = ('story_id', 'title', 'created_at')
+    search_fields = ('story_id', 'title')
+    
+admin.site.register(FavouriteStories, FavouriteStoriesAdmin)
+admin.site.register(UserProfile, userProfileAdmin)
